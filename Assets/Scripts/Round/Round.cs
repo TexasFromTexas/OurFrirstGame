7e01a706c7843a4cd51f2fea7222eed1702fe841
Fengblinx <deflfwaj@ncps.cc>
2025-12-02 19:29:40 +0800

    回合制
    需要在场景中创建一个空物体RoundController,将组件加在上面,并将Ball物体和敌人物体分别拖入BallScript和EnemyScript

---
+++ Assets/Scripts/Round/Round.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using UnityEngine;
+
+public class TurnManager : MonoBehaviour
+{
+    public enum TurnState
+    {
+        BallRound, // 现在是 BallRound
+        EnemyRound, // 现在是 EnemyRound
+        GameOver
+    }
+    public float time = 10f;
+    public TurnState currentTurnState = TurnState.BallRound;
+
+    public SlingshotBall ballScript; // 控制小球的脚本
+    public EnemyAI2D enemyScript; // 控制敌人的脚本
+
+    void Start()
+    {
+        StartCoroutine(HandleTurns()); // 启动回合控制协程
+    }
+    IEnumerator HandleTurns()
+    {
+        while (currentTurnState != TurnState.GameOver)
+        {
+            switch (currentTurnState)
+            {
+                case TurnState.BallRound: // 玩家回合
+                    StartBallRound(); // 启动 BallRound
+                    yield return new WaitUntil(() => ballScript.isStop);
+                    yield return new WaitForSeconds(1f);
+                    EndBallRound(); // 结束 BallRound
+                    break;
+
+                case TurnState.EnemyRound: // 敌人回合
+                    StartEnemyRound(); // 启动 EnemyRound
+                    yield return new WaitForSeconds(1f); 
+                    EndEnemyRound(); // 结束 EnemyRound
+                    break;
+            }
+        }
+    }
+
+    // 启动 BallRound
+    void StartBallRound()
+    {
+        // 启用小球控制脚本
+        if (ballScript != null) 
+            ballScript.enabled = true;
+
+        if (enemyScript != null)
+            enemyScript.enabled = false; // 禁用敌人脚本，防止敌人在 BallRound 内移动
+    }
+
+    // 结束 BallRound
+    void EndBallRound()
+    {
+        currentTurnState = TurnState.EnemyRound; // 转到敌人回合
+    }
+
+    // 启动 EnemyRound
+    void StartEnemyRound()
+    {
+        // 启用敌人控制脚本
+        if (enemyScript != null)
+            enemyScript.enabled = true;
+
+        if (ballScript != null)
+            ballScript.enabled = false; // 禁用小球控制脚本，防止玩家在 EnemyRound 内操作
+    }
+
+    // 结束 EnemyRound
+    void EndEnemyRound()
+    {
+        currentTurnState = TurnState.BallRound; // 转到玩家回合
+    }
+}
